6cf8e070bda4696515ae6fdb58cfb1bdcd27994a
Work desktop <the.red.lemon@yandex.ru>
2023-09-08 14:52:46 +0300

    feat: adds start component

--- src/component/start.js
+++ src/component/start.js
@@ -1,5 +1,19 @@
 class Start {
+  constructor(setting) {
+    this._setting = setting;
+    this._btnTwoPlayers = document.querySelector(this._setting.btnTwoPlayersSelector);
+    this._btnVsRobot = document.querySelector(this._setting.btnVsRobotSelector);
+  }
 
+  setEventListeners = () => {
+    this._btnTwoPlayers.addEventListener('click', () => {
+      console.log('players');
+    })
+
+    this._btnVsRobot.addEventListener('click', () => {
+      console.log('robots');
+    })
+  }
 }
 
 export default Start
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,11 @@ import './index.css';
 
 import Start from '../component/start';
 
-const start = new Start;
+const startSetting = {
+  btnTwoPlayersSelector: '.button[data-type="btn-two-players"]',
+  btnVsRobotSelector: '.button[data-type="btn-vs-robot"]',
+}
+
+const start = new Start(startSetting);
+
+start.setEventListeners();
